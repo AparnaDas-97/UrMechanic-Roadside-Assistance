a0d0088f19a49f044332a2679259593c4f4c64ae
Aparna Das <aparnakrishnadas97@gmail.com>
2021-01-10 18:07:10 +0530

    first commit

---
+++ app/src/main/java/com/example/mechfinder/ViewHolder/CategoryViewHolder.java
@@ -0,0 +1,27 @@
+package com.example.mechfinder.ViewHolder;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.mechfinder.R;
+
+public class CategoryViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+    public TextView customerRequestID,time,serviceType;
+
+
+    public CategoryViewHolder(@NonNull View itemView) {
+        super(itemView);
+        customerRequestID = (TextView)itemView.findViewById(R.id.customer);
+        time = (TextView)itemView.findViewById(R.id.time);
+        serviceType = (TextView)itemView.findViewById(R.id.service);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
